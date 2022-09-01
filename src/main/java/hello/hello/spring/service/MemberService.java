153c9c143b5ce8744914e034609e2918b249a1df
chojunho97 <whwnsgh1229@naver.com>
2022-09-01 17:55:56 +0900

    Changes

--- src/main/java/hello/hello/spring/service/MemberService.java
+++ src/main/java/hello/hello/spring/service/MemberService.java
@@ -3,13 +3,17 @@ package hello.hello.spring.service;
 import hello.hello.spring.domain.Member;
 import hello.hello.spring.repository.MemberRepository;
 import hello.hello.spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
